c70156ba66f0e308ab2734ff384f2adeed42900d
SUBHASHIS <subhashis.sarangi@gmail.com>
2023-08-12 07:45:39 +0530

    commit

---
+++ src/main/java/com/myapp/CiCdProject1Application.java
@@ -0,0 +1,13 @@
+package com.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdProject1Application.class, args);
+	}
+
+}
